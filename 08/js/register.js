a4afd595f0d345d31180c891f2cccab9f7f6b01c
Lee Joon Ho <jh.lee@spira-t.com>
2021-03-09 13:56:28 +0900

    form1

--- 08/js/register.js
+++ 08/js/register.js
@@ -3,7 +3,23 @@ function newRegister(){
     const userName = document.querySelector("#userName");
     const newText = document.createTextNode(userName.value);
     newP.appendChild(newText);
+
+    let delBttn = document.createElement('span');
+    const delText = document.createTextNode('X');
+    delBttn.setAttribute('class', 'del');
+    delBttn.appendChild(delText);
+    newP.appendChild(delBttn);
+
     let nameList = document.querySelector('#nameList');
-    nameList.appendChild(newP);
+    //nameList.appendChild(newP);
+    nameList.insertBefore(newP, nameList.childNodes[0]);
     userName.value = '';
+
+    let removeBttns = document.querySelectorAll('.del');
+    removeBttns.forEach(bttn => {
+        bttn.addEventListener('click', function(){
+            if (this.parentNode.parentNode)
+                this.parentNode.parentNode.removeChild(this.parentNode);
+        });
+    });
 }
